81b33690c0ee2dda654732d00f8e4e5c2a8d8bbf
Paulo Astorga <pauloastorga.docs@gmail.com>
2023-01-02 17:45:37 -0300

    Added Springboot dependencies

---
+++ src/main/java/cl/malbino/booking/app/AppApplication.java
@@ -0,0 +1,13 @@
+package cl.malbino.booking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
